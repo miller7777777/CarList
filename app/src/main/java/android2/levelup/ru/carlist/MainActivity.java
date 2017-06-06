caff195d5b356726ae50e1263baf874ff42ef582
miller777 <miller777@mail.ru>
2017-06-06 11:11:51 +0300

    Добавил Todo.

--- app/src/main/java/android2/levelup/ru/carlist/MainActivity.java
+++ app/src/main/java/android2/levelup/ru/carlist/MainActivity.java
@@ -3,6 +3,8 @@ package android2.levelup.ru.carlist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
